c8d34a41a2592942960f857fac6583e6fa798a95
Etan Kissling <etan@status.im>
2023-06-02 10:56:37 +0200

    extend Nim test to allocate memory
    Demonstrate how to allocate memory in the Nim code to persist state
    across multiple function calls. JavaScript receives pointers to state.

--- main.nim
+++ main.nim
@@ -1,2 +1,14 @@
-proc add(a, b: cint): cint {.exportc.} =
-  a + b
+type Foo = ref object
+  a: cint
+
+proc createFoo(a: cint): ptr Foo {.exportc.} =
+  let foo = Foo.create()
+  foo[].a = a
+  foo
+
+func getA(foo: ptr Foo): cint {.exportc.} =
+  foo[].a
+
+proc destroyFoo(foo: ptr Foo) {.exportc.} =
+  foo[].reset()
+  foo.dealloc()
